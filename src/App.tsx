0daf557fd62e8312186fff4973348bd158d57600
Marcin B <b.gataze@gmail.com>
2022-04-21 23:08:18 +0200

    Browser router and basic components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-
+import UserForm from "./UserForm";
+import RouteDets from "./RouteDets";
 
 function App() {
   return (
-    <div className="App">
-     asd
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<UserForm />} />
+          <Route path="/routeDetails" element={<RouteDets />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/UserForm.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function UserForm() {
+  return (
+    <div>
+      <Link to="/routeDetails">Show route</Link>
+    </div>
+  );
+}
